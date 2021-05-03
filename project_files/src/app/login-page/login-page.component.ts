95d429d63684109707a1c8e854ea73cbe1c24902
Goochy12-Laptop <liamgooch@live.com.au>
2021-05-03 21:56:43 +1000

    login check and reroute

--- project_files/src/app/login-page/login-page.component.ts
+++ project_files/src/app/login-page/login-page.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RetrievalService } from '../retrieval.service';
+
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -10,13 +17,21 @@ export class LoginPageComponent implements OnInit {
   username: string = null;
   password: string = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private retrievalService: RetrievalService, private router: Router,) { }
 
   ngOnInit(): void {
   }
 
   buttonOnSelect() {
-
+    this.retrievalService.authenticate(this.username, this.password).subscribe(res => {
+      if (res) {
+        if (res["exists"]) {
+          this.router.navigate(['dashboard']);
+        }
+      } else {
+        console.log(false);
+      }
+    })
   }
 
 }
