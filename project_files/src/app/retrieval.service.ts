97931da8104e70bee525a1f81c52fc20e07e2bb0
Goochy12-Laptop <liamgooch@live.com.au>
2021-05-03 23:33:33 +1000

    Update retrieval.service.ts

--- project_files/src/app/retrieval.service.ts
+++ project_files/src/app/retrieval.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
+import { User } from './types/User';
+
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
@@ -11,6 +13,24 @@ export class RetrievalService {
 
   constructor(private http: HttpClient) { }
 
+  private user: User;
+
+  createUser(id: Number) {
+    this.user = new User(id);
+  }
+
+  login() {
+
+  }
+
+  setUserDetails(id?: Number, first_name?: String, surname?: String, display_name?: String) {
+
+  }
+
+  getUser() {
+    return this.user;
+  }
+
   authenticate(username: string, password: string) {
     return this.http.get(environment.backendURL + "/authenticate", { params: new HttpParams().set("username", username).set("password", password) });
     // return new Observable((observer) => {
@@ -18,7 +38,7 @@ export class RetrievalService {
     // });
   }
 
-  getUserDetails(id: number) {
+  getUserDetails(id: Number) {
     return new Observable((observer) => {
       observer.next({ "first_name": "Liam", "surname": "Gooch", "display_name": "Liam Gooch" });
     });
