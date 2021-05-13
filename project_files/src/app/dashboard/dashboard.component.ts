42bbaec563a8b493677d59eeb94e48d4ac70a0ab
Goochy12-Laptop <liamgooch@live.com.au>
2021-05-14 01:28:17 +1000

    add cookies and get owed

--- project_files/src/app/dashboard/dashboard.component.ts
+++ project_files/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { RetrievalService } from '../retrieval.service';
 
 import { User } from '../types/User';
@@ -12,22 +13,26 @@ import { User } from '../types/User';
 export class DashboardComponent implements OnInit {
 
   user: User;
+  owed: {};
 
-  constructor(private route: ActivatedRoute, private retrievalService: RetrievalService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private retrievalService: RetrievalService, private router: Router, private cookieService: CookieService) { }
 
   ngOnInit(): void {
 
     this.user = this.retrievalService.getUser();
 
-    if (this.user == null) {
+    if (this.user) {
+      this.retrievalService.getOwed(this.user.id).subscribe(res => {
+        this.owed = res;
+      });
+    } else {
       this.router.navigate([""]);
     }
+  }
 
-    this.retrievalService.getUserDetails(this.user.id).subscribe(res => {
-      if (res) {
-        console.log(res);
-      }
-    });
+  logoff() {
+    this.cookieService.delete('userCookie');
+    this.router.navigate([""]);
   }
 
 }
