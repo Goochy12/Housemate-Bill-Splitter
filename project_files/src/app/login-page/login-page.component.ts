b0c78c131dadd2ec61912919b3c97d77d6d1f302
Goochy12-Laptop <liamgooch@live.com.au>
2021-05-15 18:33:14 +1000

    Update login-page.component.ts

--- project_files/src/app/login-page/login-page.component.ts
+++ project_files/src/app/login-page/login-page.component.ts
@@ -48,7 +48,7 @@ export class LoginPageComponent implements OnInit {
 
   getUserDetails(id: Number) {
     this.retrievalService.getUserDetails(id).subscribe(detailsRes => {
-      this.retrievalService.setUserDetails(null, detailsRes['first_name'], detailsRes['surname'], detailsRes['display_name']);
+      this.retrievalService.setUserDetails(null, detailsRes['first_name'], detailsRes['surname'], detailsRes['display_name'], detailsRes['group_id']);
     });
   }
 
