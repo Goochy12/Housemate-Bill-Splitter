43a7d6e28d942a1551014f9d0a27312baa1920b5
Goochy12-Laptop <liamgooch@live.com.au>
2021-05-22 17:15:10 +1000

    add fields for submit

--- project_files/src/app/new-record-dialog/new-record-dialog.component.ts
+++ project_files/src/app/new-record-dialog/new-record-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RetrievalService } from '../retrieval.service';
+import { User } from '../types/User';
 
 @Component({
   selector: 'app-new-record-dialog',
@@ -8,9 +10,22 @@ import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class NewRecordDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {}) { }
+  itemValue: string = null;
+  amountValue: string = null;
+  userList: [] = null;
+  user: User = null;
+  owingID: Number = null;
+  owedID: Number = null;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {}, private retrievalService: RetrievalService) { }
 
   ngOnInit(): void {
+    this.user = this.data["user"];
+    this.userList = this.data["userList"];
+  }
+
+  submit() {
+    this.retrievalService.submitNewRecord(this.user.id, this.owingID, this.owedID, this.itemValue, Number(this.amountValue));
   }
 
 }
