8d701fde77f3161b256a2862e843feb5fec4eb9c
Goochy12-Laptop <liamgooch@live.com.au>
2021-05-22 23:50:28 +1000

    add filters and total calculations

--- project_files/src/app/dashboard/dashboard.component.ts
+++ project_files/src/app/dashboard/dashboard.component.ts
@@ -15,8 +15,10 @@ import { User } from '../types/User';
 export class DashboardComponent implements OnInit {
 
   user: User;
-  owedDetailed: {};
-  owingDetailed: {};
+  owedDetailed;
+  owingDetailed;
+  newOwedDetailed;
+  newOwingDetailed;
   owedSummary: {};
   owingSummary: {};
   userList: {};
@@ -24,6 +26,10 @@ export class DashboardComponent implements OnInit {
   displayedColumnsOwed: string[] = ['from', 'item_name', 'amount'];
   displayedColumnsOwing: string[] = ['to', 'item_name', 'amount'];
   displayedColumnsAll: string[] = ['to', 'from', 'item_name', 'amount'];
+  owedFilterValue: string = null;
+  owingFilterValue: string = null;
+  owedTotal: Number = null;
+  owingTotal: Number = null;
 
   constructor(private route: ActivatedRoute, private retrievalService: RetrievalService,
     private router: Router, public dialog: MatDialog) { }
@@ -33,29 +39,37 @@ export class DashboardComponent implements OnInit {
     this.retrievalService.userObservable$.subscribe(u => {
       if (u) {
         this.user = u;
-        this.retrievalService.getOwedDetailed(this.user.id).subscribe(res => {
-          this.owedDetailed = res;
-        });
-        this.retrievalService.getOwingDetailed(this.user.id).subscribe(res => {
-          this.owingDetailed = res;
-        });
-        this.retrievalService.getOwedSummary(this.user.id).subscribe(res => {
-          this.owedSummary = res[0]["Amount"];
-        });
-        this.retrievalService.getOwingSummary(this.user.id).subscribe(res => {
-          this.owingSummary = res[0]["Amount"];
-        });
-        this.retrievalService.getAllUnpaidRecords().subscribe(res => {
-          this.allUnpaidRecords = res;
-        });
-        this.retrievalService.getUserList(this.user.groupID).subscribe(res => {
-          this.userList = res;
-        }
-        )
+        this.getAllData();
       } else {
         this.router.navigate([""]);
       }
     });
+  }
+
+  getAllData() {
+    this.retrievalService.getOwedDetailed(this.user.id).subscribe(res => {
+      this.owedDetailed = res;
+      this.newOwedDetailed = res;
+      this.calculateTotals();
+    });
+    this.retrievalService.getOwingDetailed(this.user.id).subscribe(res => {
+      this.owingDetailed = res;
+      this.newOwingDetailed = res;
+      this.calculateTotals();
+    });
+    this.retrievalService.getOwedSummary(this.user.id).subscribe(res => {
+      this.owedSummary = res[0]["Amount"];
+    });
+    this.retrievalService.getOwingSummary(this.user.id).subscribe(res => {
+      this.owingSummary = res[0]["Amount"];
+    });
+    this.retrievalService.getAllUnpaidRecords().subscribe(res => {
+      this.allUnpaidRecords = res;
+    });
+    this.retrievalService.getUserList(this.user.groupID).subscribe(res => {
+      this.userList = res;
+    }
+    )
   }
 
   logoff() {
@@ -64,11 +78,51 @@ export class DashboardComponent implements OnInit {
   }
 
   openDialog() {
-    this.dialog.open(NewRecordDialogComponent, { data: { "userList": this.userList, "user": this.user } })
+    const dialogRef = this.dialog.open(NewRecordDialogComponent, { data: { "userList": this.userList, "user": this.user } });
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        if (res.refresh) {
+          this.getAllData();
+        }
+      }
+    })
   }
 
-  getTotal() {
-    return 0;
+  calculateTotals() {
+    this.owedTotal = 0;
+    this.owingTotal = 0;
+
+    if (this.newOwedDetailed) {
+      this.newOwedDetailed.forEach(element => {
+        this.owedTotal += element.Amount;
+      });
+    }
+
+    if (this.newOwingDetailed) {
+      this.newOwingDetailed.forEach(element => {
+        this.owingTotal += element.Amount;
+      });
+    }
+  }
+
+  filter() {
+    if (this.owedFilterValue) {
+      this.newOwedDetailed = this.owedDetailed.filter(c => {
+        return c.From == this.owedFilterValue;
+      })
+    } else {
+      this.newOwedDetailed = this.owedDetailed;
+    }
+    if (this.owingFilterValue) {
+      this.newOwingDetailed = this.owingDetailed.filter(c => {
+        return c.From == this.owingFilterValue;
+      })
+    } else {
+      this.newOwingDetailed = this.owingDetailed;
+    }
+
+    this.calculateTotals();
   }
 
 }
